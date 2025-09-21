29ba99aaded782df9cd1a051f4f7f5d3541a8c15
DHRUV SAKHARE <dhruvsakhare2006@gmail.com>
2025-09-21 11:26:36 +0000

    Update sub.asm

--- sub.asm
+++ sub.asm
@@ -1,6 +1,6 @@
 section .data
-    num1 db 0x50    ; 80
-    num2 db 0x25    ; 37
+    num1 db 0x50
+    num2 db 0x25
     result db 0
 
 section .text
@@ -9,9 +9,22 @@ _start:
     mov al, [num1]
     mov bl, [num2]
     sub al, bl
+
+    mov ah, 0
+    mov bl, 10
+    div bl
+    add al, '0'
     mov [result], al
+    mov al, ah
+    add al, '0'
+    mov [result+1], al
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, result
+    mov edx, 2
+    int 0x80
 
     mov eax, 1
     xor ebx, ebx
     int 0x80
-
