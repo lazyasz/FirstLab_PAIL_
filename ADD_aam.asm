dd9179783a29ce20104a6e78022c8ace283bd945
DHRUV SAKHARE <dhruvsakhare2006@gmail.com>
2025-09-21 12:53:09 +0000

    Update ADD_aam.asm

--- ADD_aam.asm
+++ ADD_aam.asm
@@ -1,29 +1,27 @@
-global _start
-
 section .data
-     val:db 0
-     val:db 0
+val db 0, 0
 
 section .text
-     _start:
-     mov al, 73
-     mov bl, 25
-     add al, bl
-     aam
-     add al, '0'
-     mov [val], ah
+global _start
+_start:
+    mov al, 73
+    mov bl, 25
+    add al, bl
 
-     mov eax, 4
-     mov ebx, 1
-     mov ecx, vah
-     mov edx, 1
-     int 80h
+    mov ah, 0
+    mov bl, 10
+    div bl
+    add al, '0'
+    mov [val], al
+    add ah, '0'
+    mov [val+1], ah
 
-     mov eax, 4
-     mov ebx, 1
-     mov ecx, val
-     mov edx, 1
-     int 80h
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, val
+    mov edx, 2
+    int 0x80
 
-     mov eax, 1
-     int 80h
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
