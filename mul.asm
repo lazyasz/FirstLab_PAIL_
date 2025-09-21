45bda104e002455573cabb0b378e7d37ceeec31e
DHRUV SAKHARE <dhruvsakhare2006@gmail.com>
2025-09-21 11:31:32 +0000

    Update mul.asm

--- mul.asm
+++ mul.asm
@@ -8,8 +8,22 @@ global _start
 _start:
     mov al, [num1]
     mov bl, [num2]
-    mul bl         ; unsigned multiply: AX = AL * BL
-    mov [result], al   ; low 8-bit result
+    mul bl
+
+    mov ah, 0
+    mov bl, 10
+    div bl
+    add al, '0'
+    mov [result], al
+    mov al, ah
+    add al, '0'
+    mov [result+1], al
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, result
+    mov edx, 2
+    int 0x80
 
     mov eax, 1
     xor ebx, ebx
