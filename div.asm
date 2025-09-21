45d173a2ca7cdee81926022f1fcd12915a20b6b4
DHRUV SAKHARE <dhruvsakhare2006@gmail.com>
2025-09-21 11:34:56 +0000

    Update div.asm

--- div.asm
+++ div.asm
@@ -9,12 +9,36 @@ global _start
 _start:
     mov al, [num1]
     mov bl, [num2]
-    xor ah, ah      ; clear AH for 8-bit division
-    div bl          ; AL = quotient, AH = remainder
+    xor ah, ah
+    div bl
+
+    mov ah, 0
+    mov bl, 10
+    div bl
+    add al, '0'
     mov [quotient], al
-    mov [remainder], ah
+    mov al, ah
+    add al, '0'
+    mov [quotient+1], al
+
+    mov ah, 0
+    mov al, [remainder]
+    div bl
+    add al, '0'
+    mov [remainder], al
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, quotient
+    mov edx, 2
+    int 0x80
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, remainder
+    mov edx, 1
+    int 0x80
 
     mov eax, 1
     xor ebx, ebx
     int 0x80
-
